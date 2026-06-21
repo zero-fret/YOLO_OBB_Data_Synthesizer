d47f5228d9d8a59d1e022709aa972b834f95dbdd
Zero Fret <zerofret0@outlook.com>
2026-06-21 08:36:13 +0800

    Update model path in predict.py to best.pt from test-1

--- predict.py
+++ predict.py
@@ -7,14 +7,14 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
 if __name__ == '__main__':
-    model = YOLO('./runs/obb/result/test-24/weights/best.pt')
+    model = YOLO('./runs/obb/result/test-1/weights/best.pt')
     print(f"Model Type: {type(model.model)}")
     print(f"Parameters: {sum(p.numel() for p in model.model.parameters())}")
     
     args = dict(
         imgsz=1280,
         conf=0.1,
-        model="./runs/obb/result/test-6/weights/best.pt",
+        model="./runs/obb/result/test-1/weights/best.pt",
         source="./test",
         project="./predict",
     )
